2fcc3dbf0e249d173f26e7a5d6294998566fee3f
Pierre Payet <ext.kleegroup.ppayet@grandlyon.com>
2024-12-17 16:40:48 +0100

    fix: solve gilded rose kata

---
+++ README.md
@@ -0,0 +1,31 @@
+# Gilded Rose Refactoring Kata in TypeScript with Jest
+
+[Link to the github repo of the kata topic](https://github.com/emilybache/GildedRose-Refactoring-Kata)
+
+## Getting started
+
+Install dependencies
+
+```sh
+npm install
+```
+
+## Run the unit tests from the Command-Line
+
+To run all tests
+
+```sh
+npm test
+```
+
+To run all tests in watch mode
+
+```sh
+npm run test:watch
+```
+
+To generate test coverage report
+
+```sh
+npm run test:coverage
+```
---
+++ src/gilded_rose.ts
@@ -0,0 +1,131 @@
+interface ItemInterface {
+  name: string;
+  sellIn: number;
+  quality: number;
+
+  updateQuality(): void;
+}
+
+export class Item implements ItemInterface {
+  static maxQuality = 50;
+
+  constructor(public name: string, public sellIn: number, public quality: number) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality > 0 ? quality : 0;
+  }
+
+  public updateQuality() {
+    this.sellIn--;
+
+    this.decreaseQuality();
+
+    if (this.isSellInExpired) {
+      this.decreaseQuality()
+    }
+  }
+
+  public decreaseQuality() {
+    if (this.quality > 0) {
+      this.quality--;
+    }
+  }
+
+  public increaseQuality() {
+    if (this.isQualityInferiorToMaxQuality) {
+      this.quality++;
+    }
+  }
+
+  get isQualityInferiorToMaxQuality(): boolean {
+    return this.quality < Item.maxQuality;
+  }
+
+  get isSellInExpired() {
+    return this.sellIn < 0;
+  }
+}
+
+export class Sulfuras extends Item {
+  constructor(public name: string, public sellIn: number, public quality: number) {
+    super(name, sellIn, quality);
+  }
+
+  public override updateQuality() {}
+}
+
+export class AgedBrie extends Item {
+  constructor(public name: string, public sellIn: number, public quality: number) {
+    super(name, sellIn, quality);
+  }
+
+  public override updateQuality() {
+    this.sellIn--;
+
+    this.increaseQuality();
+
+    if (this.isSellInExpired) {
+      this.increaseQuality();
+    }
+  }
+}
+
+export class BackstagePasses extends Item {
+  private sellInFiveDays = 5;
+  private sellInTenDays = 10;
+
+  constructor(public name: string, public sellIn: number, public quality: number) {
+    super(name, sellIn, quality);
+  }
+
+  public override updateQuality() {
+    this.sellIn--;
+
+    this.increaseQuality();
+
+    if (this.sellIn <= this.sellInTenDays) {
+      this.increaseQuality();
+    }
+
+    if (this.sellIn <= this.sellInFiveDays) {
+      this.increaseQuality();
+    }
+
+    if (this.isSellInExpired) {
+      this.quality = 0;
+    }
+  }
+}
+
+export class Conjured extends Item {
+  constructor(public name: string, public sellIn: number, public quality: number) {
+    super(name, sellIn, quality);
+  }
+
+  public override updateQuality() {
+    this.sellIn--;
+
+    this.decreaseQuality();
+
+    if (this.isSellInExpired) {
+      this.decreaseQuality();
+    }
+  }
+
+  public override decreaseQuality() {
+    super.decreaseQuality();
+    super.decreaseQuality();
+  }
+}
+
+export class Shop {
+  constructor(public items: Array<ItemInterface> = []){
+    this.items = items;
+  }
+
+  updateQuality() {
+    this.items.forEach(item => item.updateQuality());
+
+    return this.items;
+  }
+}
